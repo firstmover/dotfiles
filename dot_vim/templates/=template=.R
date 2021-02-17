f5e38b89c97dbe99b8c87e30593df6bb547eced8
Yingcheng Liu <liuyc@mit.edu>
2021-02-17 03:18:43 -0500

    Add R tempalte

---
+++ dot_vim/templates/=template=.R
@@ -0,0 +1,12 @@
+#! /usr/bin/env Rscript
+# -*- coding: utf-8 -*-
+# 
+# Author : %USER%
+# Email  : %MAIL%
+# Date   : %MONTH%/%DAY%/%YEAR%
+#
+# Distributed under terms of the %LICENSE% license.
+
+## 
+%HERE%
+##
